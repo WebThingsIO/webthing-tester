c1d963d737da196c1e49df7032a37f21f6f246d0
Michael Stegeman <michael@stegeman.me>
2018-11-29 09:50:12 -0700

    Add link arrays to properties/actions/events.

--- test-client.py
+++ test-client.py
@@ -118,7 +118,8 @@ def run_client():
     assert body['properties']['on']['label'] == 'On/Off'
     assert body['properties']['on']['type'] == 'boolean'
     assert body['properties']['on']['description'] == 'Whether the lamp is turned on'
-    assert body['properties']['on']['href'] == _PATH_PREFIX + '/properties/on'
+    assert len(body['properties']['on']['links']) == 1
+    assert body['properties']['on']['links'][0]['href'] == _PATH_PREFIX + '/properties/on'
     assert body['properties']['brightness']['@type'] == 'BrightnessProperty'
     assert body['properties']['brightness']['label'] == 'Brightness'
     assert body['properties']['brightness']['type'] == 'integer'
@@ -126,7 +127,8 @@ def run_client():
     assert body['properties']['brightness']['minimum'] == 0
     assert body['properties']['brightness']['maximum'] == 100
     assert body['properties']['brightness']['unit'] == 'percent'
-    assert body['properties']['brightness']['href'] == _PATH_PREFIX + '/properties/brightness'
+    assert len(body['properties']['brightness']['links']) == 1
+    assert body['properties']['brightness']['links'][0]['href'] == _PATH_PREFIX + '/properties/brightness'
     assert body['actions']['fade']['label'] == 'Fade'
     assert body['actions']['fade']['description'] == 'Fade the lamp to a given level'
     assert body['actions']['fade']['input']['type'] == 'object'
@@ -137,11 +139,13 @@ def run_client():
     assert body['actions']['fade']['input']['properties']['duration']['type'] == 'integer'
     assert body['actions']['fade']['input']['properties']['duration']['minimum'] == 1
     assert body['actions']['fade']['input']['properties']['duration']['unit'] == 'milliseconds'
-    assert body['actions']['fade']['href'] == _PATH_PREFIX + '/actions/fade'
+    assert len(body['actions']['fade']['links']) == 1
+    assert body['actions']['fade']['links'][0]['href'] == _PATH_PREFIX + '/actions/fade'
     assert body['events']['overheated']['type'] == 'number'
     assert body['events']['overheated']['unit'] == 'degree celsius'
     assert body['events']['overheated']['description'] == 'The lamp has exceeded its safe operating temperature'
-    assert body['events']['overheated']['href'] == _PATH_PREFIX + '/events/overheated'
+    assert len(body['events']['overheated']['links']) == 1
+    assert body['events']['overheated']['links'][0]['href'] == _PATH_PREFIX + '/events/overheated'
     assert len(body['links']) >= 4
     assert body['links'][0]['rel'] == 'properties'
     assert body['links'][0]['href'] == _PATH_PREFIX + '/properties'
